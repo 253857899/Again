92e7d412c3a3a6c35641909ab21afd9b5da637d7
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-29 21:51:52 +0800

    xml

--- Re_Again/src/main/java/dao/Type_dao.java
+++ Re_Again/src/main/java/dao/Type_dao.java
@@ -2,14 +2,13 @@ package dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+
+
 import org.springframework.stereotype.Repository;
 
 import entry.Type;
 
 @Repository
 public interface Type_dao {
-	@Select("select * from type ${where}")
-	public List<Type> select(@Param(value = "where" ) String where);
+	public List<Type> select(String where);
 }
