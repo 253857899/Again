dcc1f0142e8c42af3287cb896ba202e6916ad7d3
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-29 17:14:49 +0800

    controller_1

--- Re_Again/src/main/java/controller/Test1.java
+++ Re_Again/src/main/java/controller/Test1.java
@@ -11,10 +11,7 @@ public class Test1 {
 		@RequestMapping("index")
 		public String index(@RequestParam(name="poet") String name,ModelMap m) {
 			m.put("poet", name);
-			System.out.println("我永恒的灵魂");
-			System.out.println("注视着你的心");
-			System.out.println("纵然黑夜孤寂");
-			System.out.println("白昼如焚");
+			System.out.println("控制器启动!!!!!!!!!!!!!!!!!!!");
 			return "index";
 		}
 }
