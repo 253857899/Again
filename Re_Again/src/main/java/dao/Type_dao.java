147e385a1dfbaa15cd8f83d60c7badb3e001fd89
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-30 10:46:59 +0800

    XML 模式

--- Re_Again/src/main/java/dao/Type_dao.java
+++ Re_Again/src/main/java/dao/Type_dao.java
@@ -2,13 +2,12 @@ package dao;
 
 import java.util.List;
 
-
-
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import entry.Type;
 
 @Repository
 public interface Type_dao {
-	public List<Type> select(String where);
+	public List<Type> select(@Param("name")String where);
 }
