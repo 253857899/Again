563d1ae3111667fab4e114b15155daee959c3e68
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-30 09:35:41 +0800

    @ 注解模式

--- Re_Again/src/main/java/service_Impl/TypeServiceImpl.java
+++ Re_Again/src/main/java/service_Impl/TypeServiceImpl.java
@@ -1,10 +1,7 @@
 package service_Impl;
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import dao.Type_dao;
