8df7032af6fa08b9303cab5ddbeb03aca9f33ee7
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-09-04 21:11:01 +0800

    泛型

--- Re_Again/src/main/java/dao/Book_dao.java
+++ Re_Again/src/main/java/dao/Book_dao.java
@@ -2,7 +2,6 @@ package dao;
 
 import java.util.List;
 
-
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
@@ -10,28 +9,19 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
-
 import entry.Book;
-
-
+import entry.Type;
 
 @Repository
 public interface Book_dao {
-	@Select("select * from book")
+	@Select("SELECT * FROM  book " )
 	public List<Book> select();
-	
-	@Select("select count(1) from book inner join type on type.bookid=book.typeid ${where}")
-	public Integer search(@Param(value = "where") String where);
-	
-	@Select("select Type.*,book.name bookname from book inner join Type on Type.bookid=book.typeid ${where} ${limit}")
-	public List<Book> select(@Param(value = "where") String where, @Param(value = "limit") String limit);
-	
-	@Insert("insert into Book(name,status,bookid) values(#{name},#{status},#{bookid})")
-	public Integer insert(Book b);
-	
-	@Update("update Book set name=#{name},status=#{status},bookid=#{bookid} where id=#{id}")
-	public Integer update(Book b);
-	
-	@Delete("delete from Book  where id=#{id}")
-	public Integer delete(Book b);
+	@Delete("delete from type where id = #{id} ")
+	public void delete(int id);
+	@Insert("insert into book(name,status,typeid) value(#{name},#{status},#{typeid})")
+	public void insert(Book t);
+	@Update("update book set name=#{name},status=#{status},typeid=#{typeid} where id=#{id}")
+	public void update(Book t);
+	@Select("select * from book where id = #{id}")
+	public Book selectById(int id);
 }
